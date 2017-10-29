53b1c8c5622caac09485a4e4079cd48d39a5f577
Amaury Fauchille <amaury.fauchille@epita.fr>
2017-10-29 11:27:41 +0100

    Add more test functions and improve swarm

--- functions.py
+++ functions.py
@@ -1,4 +1,54 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 import numpy as np
+from numpy import power, sin, floor
 
-def DeJongF1(x):
-    return np.dot(x, x)
+def Michalewicz(X):
+    res = 0
+    for i, x in enumerate(X):
+        res += sin(x) * power(sin((i + 1) * x * x / np.pi), 20)
+    return res
+
+# f([0, 0, 0]) = 0
+def DeJongF1(X):
+    return np.dot(X, X)
+
+# D = 2
+# f([1, 1, 1]) = 0
+def DeJongF2(X):
+    return 100 * (X[1] * X[1] - X[0]) + 1 - X[0]
+
+def DeJongF3(X):
+    res = 0
+    for x in X:
+        res += floor(x)
+    return res
+
+#TODO
+# D = 2
+#f([-1, 0]) = 3
+def GP(X):
+    f1 = (1 + power(X[0] + X[1] + 1, 2) * (19 - 14 * X[0] + 13 * X[0] * X[0] - 14 * X[1] + 6 * X[0] * X[1] + 3 * X[1] * X[1]))
+    f2 = (30 + power(2 * X[0] - 3 * X[1], 2) * (18 - 32 * X[0] + 12 * X[0] * X[0] - 48 * X[1] - 36 * X[0] * X[1] + 27 * X[1] * X[1]))
+    return f1 * f2
+
+def Rosenbrock(X):
+    res = 0
+    for i in range(len(X) - 1):
+        x = X[i]
+        xp = X[i + 1]
+        res += 100 * power(x * x - xp, 2) + x - 1
+    return res
+
+
+def plot3D(f, low_pos, high_pos):
+    x = np.linspace(low_pos, high_pos, 100)
+    y = np.linspace(low_pos, high_pos, 100)
+    z = [f([xi, yi]) for xi, yi in zip(x, y)]
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(x, y, z)
+    plt.show()
+
+#print(Rosenbrock([0, 1, 2, 3, 4, 5]))
+plot3D(DeJongF1, -5.12, 5.12)
--- swarm.py
+++ swarm.py
@@ -1,39 +1,72 @@
 import numpy as np
 from functions import *
 import sys
+from random import uniform
 
 class Agent():
+
+    best_x = []
+    best_y = sys.maxsize
+    best_best_x = []
+    best_best_y = sys.maxsize
+
     def __init__(self, D, low_pos, high_pos):
         self.reset(D, low_pos, high_pos)
 
     def reset(self, D, low_pos, high_pos):
         self.x = np.random.uniform(low_pos, high_pos, D)
         self.v = np.zeros(D)
+        self.low_pos = low_pos
+        self.high_pos = high_pos
 
-    def update(self):
-        pass
+    def init(D):
+        Agent.best_x = np.zeros(D)
+        Agent.best_best_x = np.zeros(D)
+
+    def update(self, f, c, phi1, phi2):
+        c2 = uniform(0, phi1)
+        c3 = uniform(0, phi2)
+        self.v = c * self.v + c2 * (Agent.best_x - self.x) + c3 * (Agent.best_best_x - self.x)
+
+        # Could update v to real speed in case of clipping
+        self.x += self.v
+        self.x = np.clip(self.x, self.low_pos, self.high_pos)
+
+        # Update best values
+        y = f(self.x)
+        if y < Agent.best_y:
+            Agent.best_x = self.x
+            Agent.best_y = y
+
+    def update_best():
+        if Agent.best_y < Agent.best_best_y:
+            Agent.best_best_x = Agent.best_x
+            Agent.best_best_y = Agent.best_y
 
 class Swarm(object):
     def __init__(self, f, D, low_pos, high_pos, nb_iter, nb_agents, c, phi1, phi2):
         self.f = f
         self.D = D
+        self.low_pos = low_pos
+        self.high_pos = high_pos
         self.nb_iter = nb_iter
-        #self.nb_agents = nb_agents
+        self.nb_agents = nb_agents
         self.c = c
         self.phi1 = phi1
         self.phi2 = phi2
 
         self.agents = [Agent(D, low_pos, high_pos) for i in range(nb_agents)]
+        Agent.init(D)
 
     def resolve(self):
-        min_value = sys.maxsize
-        for a in self.agents:
-            a.update()
-            y = self.f(a.x)
-            if y < min_value:
-                min_value = y
-        print("Minimum value found for {} in {} dimensions:\n{:.4f}".format(self.f.__name__, self.D, min_value))
-        print("Parameters are:\n")
+        for i in range(self.nb_iter):
+            for a in self.agents:
+                a.update(self.f, self.c, self.phi1, self.phi2)
+            Agent.update_best()
+            print("Best value at iteration #{}: f({}) = {}".format(i, Agent.best_x, Agent.best_y))
+        print("Minimum value found for {} in {} dimensions:\nf({}) = {:.4f}".format(self.f.__name__, self.D, Agent.best_best_x, Agent.best_best_y))
+        print("\nX boundaries:\n- Low: {}\n- High: {}\n\nSwarm:\n- Number of agents: {}\n- Number of iterations: {}\n\nConstants:\n- c: {}\n- Phi1: {}\n- Phi2: {}"
+                .format(self.low_pos, self.high_pos, self.nb_agents, self.nb_iter, self.c, self.phi1, self.phi2))
 
-s = Swarm(DeJongF1, 3, -5.12, 5.12, 2000, 50, 1, 2, 2)
+s = Swarm(DeJongF1, 3, -5.12, 5.12, 100, 10, 1, 2, 2)
 s.resolve()
